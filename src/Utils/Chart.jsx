52994b7f333ca9fe02adc4d8386575f98ac93c18
Koos85 <koosjoosten@gmail.com>
2017-02-20 14:51:19 +0100

    .

--- src/Utils/Chart.jsx
+++ src/Utils/Chart.jsx
@@ -240,7 +240,6 @@ class Chart extends React.Component {
 
         tmp.selectAll('rect')
             .attr('height', this.height - this.props.marginTop - this.props.marginBottom)
-            .attr('class', 'zoombox')
             .on('contextmenu', zoomOut);
     }
 
