19a1af9eecd0be96b208836dbd508e10ab07bfc1
Jeroen van der Heijden <joente@gmail.com>
2017-03-09 10:19:59 +0100

    Upd version

--- lib/version.py
+++ lib/version.py
@@ -1,4 +1,4 @@
-__version_info__ = (1, 0, 4)
+__version_info__ = (1, 0, 5)
 __version__ = '.'.join(map(str, __version_info__))
 __maintainer__ = 'Jeroen van der Heijden'
 __email__ = 'jeroen@transceptor.technology'
