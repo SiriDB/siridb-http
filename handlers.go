694f796c504e264c34060da797f5cb670df4cc77
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-05-24 14:04:14 +0200

    Added qpack and msgpack support

--- handlers.go
+++ handlers.go
@@ -3,36 +3,47 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
+	msgpack "gopkg.in/vmihailenco/msgpack.v2"
+
+	"reflect"
+
 	"github.com/googollee/go-socket.io"
+	qpack "github.com/transceptor-technology/go-qpack"
 	siridb "github.com/transceptor-technology/go-siridb-connector"
 )
 
 type tDb struct {
-	Dbname        string `json:"dbname"`
-	TimePrecision string `json:"timePrecision"`
-	Version       string `json:"version"`
-	HTTPServer    string `json:"httpServer"`
+	Dbname        string `json:"dbname" qp:"dbname" msgpack:"dbname"`
+	TimePrecision string `json:"timePrecision" qp:"timePrecision" msgpack:"timePrecision"`
+	Version       string `json:"version" qp:"version" msgpack:"version"`
+	HTTPServer    string `json:"httpServer" qp:"httpServer" msgpack:"httpServer"`
 }
 
 type tAuthFetch struct {
-	User         interface{} `json:"user" qp:"user"`
-	AuthRequired bool        `json:"authRequired" qp:"authRequired"`
+	User         interface{} `json:"user" qp:"user" msgpack:"user"`
+	AuthRequired bool        `json:"authRequired" qp:"authRequired" msgpack:"authRequired"`
 }
 
 type tAuthLoginReq struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" qp:"username" msgpack:"username"`
+	Password string `json:"password" qp:"password" msgpack:"password"`
 }
 
 type tAuthLoginRes struct {
-	User string `json:"user"`
+	User string `json:"user" qp:"user" msgpack:"user"`
 }
 
 type tAuthLogoff struct {
-	User interface{} `json:"user"`
+	User interface{} `json:"user" qp:"user" msgpack:"user"`
+}
+
+type tQuery struct {
+	Query   string      `json:"query" qp:"query" msgpack:"query"`
+	Timeout interface{} `json:"timeout" qp:"timeout" msgpack:"timeout"`
 }
 
 func checkBasicAuth(r *http.Request) (conn *Conn) {
@@ -79,19 +90,19 @@ func getConnByHTTP(w http.ResponseWriter, r *http.Request) *Conn {
 	sess, err := base.gsessions.SessionStart(w, r)
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 	} else {
 		user, ok := sess.Get("user").(string)
 		if ok {
 			conn = getConnByUser(user)
 			if conn == nil {
-				http.Error(
+				sendError(
 					w,
 					fmt.Sprintf("no connection for user '%s' found, please try to login again", user),
 					http.StatusUnauthorized)
 			}
 		} else if base.reqAuth {
-			http.Error(w, "not authenticated", http.StatusUnauthorized)
+			sendError(w, "not authenticated", http.StatusUnauthorized)
 		} else {
 			conn = &base.connections[0]
 		}
@@ -99,11 +110,37 @@ func getConnByHTTP(w http.ResponseWriter, r *http.Request) *Conn {
 	return conn
 }
 
+func sendError(w http.ResponseWriter, err string, code int) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	http.Error(w, err, code)
+}
+
 func sendJSON(w http.ResponseWriter, data interface{}) {
 	if b, err := json.Marshal(data); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.Write(b)
+	}
+}
+
+func sendQPack(w http.ResponseWriter, data interface{}) {
+	if b, err := qpack.Pack(data); err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/x-qpack; charset=UTF-8")
+		w.Write(b)
+	}
+}
+
+func sendMsgPack(w http.ResponseWriter, data interface{}) {
+	if b, err := msgpack.Marshal(data); err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/x-msgpack; charset=UTF-8")
 		w.Write(b)
 	}
 }
@@ -147,7 +184,7 @@ func addConnection(username, password string) (*Conn, error) {
 }
 
 func handlerNotFound(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "404 not found", http.StatusNotFound)
+	sendError(w, "404 not found", http.StatusNotFound)
 }
 
 func onDbInfo(so *socketio.Socket) (int, string) {
@@ -165,7 +202,39 @@ func handlerDbInfo(w http.ResponseWriter, r *http.Request) {
 		TimePrecision: base.timePrecision,
 		Version:       base.version,
 		HTTPServer:    AppVersion}
-	sendJSON(w, db)
+	sendData(w, r, db)
+}
+
+func sendData(w http.ResponseWriter, r *http.Request, data interface{}) {
+	contentType := r.Header.Get("Content-type")
+
+	switch strings.ToLower(contentType) {
+	case "application/json":
+		sendJSON(w, data)
+	case "application/x-qpack":
+		sendQPack(w, data)
+	case "application/x-msgpack":
+		sendMsgPack(w, data)
+	default:
+		sendError(w, fmt.Sprintf("unsupported content-type: %s", contentType), http.StatusUnsupportedMediaType)
+	}
+}
+
+func readBody(w http.ResponseWriter, r *http.Request, v interface{}) error {
+	contentType := r.Header.Get("Content-type")
+
+	switch strings.ToLower(contentType) {
+	case "application/json":
+		return readJSON(w, r, &v)
+	case "application/x-qpack":
+		return readQPack(w, r, &v)
+	case "application/x-msgpack":
+		return readMsgPack(w, r, &v)
+	default:
+		err := fmt.Errorf("unsupported content-type: %s", contentType)
+		sendError(w, err.Error(), http.StatusUnsupportedMediaType)
+		return err
+	}
 }
 
 func onAuthFetch(so *socketio.Socket) (int, string) {
@@ -185,7 +254,7 @@ func handlerAuthFetch(w http.ResponseWriter, r *http.Request) {
 	sess, err := base.gsessions.SessionStart(w, r)
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -194,7 +263,7 @@ func handlerAuthFetch(w http.ResponseWriter, r *http.Request) {
 	} else if !base.reqAuth {
 		authFetch.User = base.connections[0].user
 	}
-	sendJSON(w, authFetch)
+	sendData(w, r, authFetch)
 }
 
 func onAuthLogin(so *socketio.Socket, req string) (int, string) {
@@ -229,36 +298,32 @@ func handlerAuthLogin(w http.ResponseWriter, r *http.Request) {
 
 	sess, err := base.gsessions.SessionStart(w, r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	decoder := json.NewDecoder(r.Body)
-	err = decoder.Decode(&authLoginReq)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := readBody(w, r, &authLoginReq); err != nil {
 		return
 	}
 
 	if conn := getConnByUser(authLoginReq.Username); conn != nil {
 		if authLoginReq.Password != conn.password {
-			http.Error(w, "Username or password incorrect", http.StatusUnprocessableEntity)
+			sendError(w, "Username or password incorrect", http.StatusUnprocessableEntity)
 			return
 		}
 	} else if base.multiUser {
 		if _, err := addConnection(authLoginReq.Username, authLoginReq.Password); err != nil {
-			http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+			sendError(w, err.Error(), http.StatusUnprocessableEntity)
 			return
 		}
 	} else {
-		http.Error(w, "Multiple user login is not allowed", http.StatusUnprocessableEntity)
+		sendError(w, "Multiple user login is not allowed", http.StatusUnprocessableEntity)
 		return
 	}
 
 	sess.Set("user", authLoginReq.Username)
 	authLoginRes := tAuthLoginRes{User: authLoginReq.Username}
-	sendJSON(w, authLoginRes)
+	sendData(w, r, authLoginRes)
 }
 
 func onAuthLogout(so *socketio.Socket) (int, string) {
@@ -272,16 +337,16 @@ func handlerAuthLogout(w http.ResponseWriter, r *http.Request) {
 
 	sess, err := base.gsessions.SessionStart(w, r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	if err = sess.Flush(); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		sendError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	sendJSON(w, authLogoff)
+	sendData(w, r, authLogoff)
 }
 
 func onQuery(so *socketio.Socket, req string) (int, string) {
@@ -290,12 +355,7 @@ func onQuery(so *socketio.Socket, req string) (int, string) {
 		return http.StatusUnauthorized, err.Error()
 	}
 
-	type Query struct {
-		Query   string      `json:"query"`
-		Timeout interface{} `json:"timeout"`
-	}
-
-	var query Query
+	var query tQuery
 
 	if err = json.Unmarshal([]byte(req), &query); err != nil {
 		return http.StatusInternalServerError, err.Error()
@@ -316,19 +376,10 @@ func onQuery(so *socketio.Socket, req string) (int, string) {
 
 func handlerQuery(w http.ResponseWriter, r *http.Request) {
 	if conn := getConnByHTTP(w, r); conn != nil {
-		type Query struct {
-			Query   string      `json:"query"`
-			Timeout interface{} `json:"timeout"`
-		}
+		var query tQuery
 
-		var query Query
-
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&query)
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		if err := readBody(w, r, &query); err != nil {
+			return // error is send by the readBody function
 		}
 
 		timeout, ok := query.Timeout.(uint16)
@@ -338,11 +389,11 @@ func handlerQuery(w http.ResponseWriter, r *http.Request) {
 
 		res, err := conn.client.Query(query.Query, timeout)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			sendError(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		sendJSON(w, res)
+		sendData(w, r, res)
 	}
 }
 
@@ -353,7 +404,6 @@ func onInsert(so *socketio.Socket, req string) (int, string) {
 	}
 
 	var insert interface{}
-
 	decoder := json.NewDecoder(strings.NewReader(req))
 	decoder.UseNumber()
 
@@ -369,24 +419,84 @@ func onInsert(so *socketio.Socket, req string) (int, string) {
 	return retJSON(res)
 }
 
+func readJSON(w http.ResponseWriter, r *http.Request, v *interface{}) error {
+	decoder := json.NewDecoder(r.Body)
+	decoder.UseNumber()
+	if err := decoder.Decode(v); err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	return nil
+}
+
+func readMsgPack(w http.ResponseWriter, r *http.Request, v *interface{}) error {
+	decoder := msgpack.NewDecoder(r.Body)
+	if err := decoder.Decode(v); err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+	return nil
+}
+
+func readQPack(w http.ResponseWriter, r *http.Request, v *interface{}) error {
+	b, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	res, err := qpack.Unpack(b, qpack.QpFlagStringKeysOnly)
+	if err != nil {
+		sendError(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	iface, ok := (*v).(*interface{})
+	if ok {
+		*iface = res
+		return nil
+	}
+
+	m, ok := res.(map[string]interface{})
+	if !ok {
+		err := fmt.Errorf("expecting a map for a non interface{} value")
+		sendError(w, err.Error(), http.StatusInternalServerError)
+		return err
+	}
+
+	e := reflect.ValueOf(*v).Elem()
+	t := e.Type()
+	n := t.NumField()
+
+	for i := 0; i < n; i++ {
+		field := t.Field(i)
+		fn := field.Tag.Get("qp")
+		if len(fn) == 0 {
+			fn = field.Name
+		}
+		val, ok := m[fn]
+		if ok {
+			e.Field(i).Set(reflect.ValueOf(val))
+		}
+	}
+
+	return nil
+}
+
 func handlerInsert(w http.ResponseWriter, r *http.Request) {
 	if conn := getConnByHTTP(w, r); conn != nil {
 		var insert interface{}
-
-		decoder := json.NewDecoder(r.Body)
-		decoder.UseNumber()
-
-		if err := decoder.Decode(&insert); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		if err := readBody(w, r, &insert); err != nil {
+			return // error is send by the readBody function
 		}
 
 		res, err := conn.client.Insert(insert, base.insertTimeout)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			sendError(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		sendJSON(w, res)
+		sendData(w, r, res)
 	}
 }
