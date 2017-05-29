5c4284e06c103676e1be0d1fa553a1c1c258c41e
Jeroen van der Heijden <jeroen@transceptor.technology>
2017-05-29 12:05:13 +0200

    Added csv handler

--- csvhandler.go
+++ csvhandler.go
@@ -54,6 +54,10 @@ func queryToCsv(lines *[]string, v interface{}) error {
 		return fmt.Errorf("got an unexpected map")
 	}
 
+	if err := tryTimeIt(lines, m); err != nil {
+		return err
+	}
+
 	if stop, err := tryList(lines, m); stop {
 		return err
 	}
@@ -62,11 +66,106 @@ func queryToCsv(lines *[]string, v interface{}) error {
 		return err
 	}
 
+	if stop, err := tryShow(lines, m); stop {
+		return err
+	}
+
+	if stop, err := tryMsg(lines, m); stop {
+		return err
+	}
+
+	if stop, err := trySelect(lines, m); stop {
+		return err
+	}
+
 	return fmt.Errorf("cannot convert query data to csv")
 }
 
+func tryTimeIt(lines *[]string, m map[string]interface{}) error {
+	if timeit, ok := m["__timeit__"]; ok {
+		*lines = append(*lines, "server name,query time")
+
+		if reflect.TypeOf(timeit).Kind() != reflect.Slice {
+			return fmt.Errorf("timeit: __timeit__ not a slice")
+		}
+
+		slice := reflect.ValueOf(timeit)
+		n := slice.Len()
+		for i := 0; i < n; i++ {
+			res := slice.Index(i).Interface()
+			resmap, ok := res.(map[string]interface{})
+			if !ok {
+				return fmt.Errorf("timeit: no map")
+			}
+			server, ok := resmap["server"]
+			if !ok {
+				return fmt.Errorf("timeit: no server")
+			}
+			time, ok := resmap["time"]
+			if !ok {
+				return fmt.Errorf("timeit: no time")
+			}
+			*lines = append(*lines, fmt.Sprintf(
+				"%s,%s",
+				escapeCsv(fmt.Sprint(server)),
+				escapeCsv(fmt.Sprint(time))))
+		}
+		*lines = append(*lines, "")
+
+		delete(m, "__timeit__")
+	}
+	return nil
+}
+
+func trySelect(lines *[]string, m map[string]interface{}) (bool, error) {
+	for sn, pnts := range m {
+
+		if reflect.TypeOf(pnts).Kind() != reflect.Slice {
+			return false, fmt.Errorf("select: points not a slice")
+		}
+
+		points := reflect.ValueOf(pnts)
+		n := points.Len()
+		for i := 0; i < n; i++ {
+			pnt := points.Index(i).Interface()
+			if reflect.TypeOf(pnt).Kind() != reflect.Slice {
+				return false, fmt.Errorf("select: point not a slice")
+			}
+			point := reflect.ValueOf(pnt)
+			if point.Len() != 2 {
+				return false, fmt.Errorf("select: point should be of len 2")
+			}
+			*lines = append(*lines, fmt.Sprintf(
+				`%s,%v,%s`,
+				escapeCsv(sn),
+				point.Index(0),
+				escapeCsv(fmt.Sprint(point.Index(1)))))
+		}
+	}
+	return true, nil
+}
+
+func tryMsg(lines *[]string, m map[string]interface{}) (bool, error) {
+	options := [4]string{
+		"success_msg",
+		"error_msg",
+		"help",
+		"motd"}
+	for _, option := range options {
+		if message, ok := m[option]; ok {
+			msg, ok := message.(string)
+			if ok {
+				*lines = append(*lines, fmt.Sprintf(`%s,%s`, option, escapeCsv(msg)))
+				return true, nil
+			}
+		}
+	}
+	return false, fmt.Errorf("no message found")
+}
+
 func tryShow(lines *[]string, m map[string]interface{}) (bool, error) {
-	var data, item, name, value interface{}
+	var data, name, value interface{}
+	var item map[string]interface{}
 	var ok bool
 
 	if data, ok = m["data"]; !ok {
@@ -96,13 +195,18 @@ func tryShow(lines *[]string, m map[string]interface{}) (bool, error) {
 		if value, ok = item["value"]; !ok {
 			return false, fmt.Errorf("show: value not fount in item")
 		}
-		s =
-	}
 
+		temp[i] = fmt.Sprintf(`%v,%s`, name, escapeCsv(fmt.Sprint(value)))
+	}
+	for i := 0; i < n; i++ {
+		*lines = append(*lines, temp[i])
+	}
+	return true, nil
 }
 
 func tryCount(lines *[]string, m map[string]interface{}) (bool, error) {
-	cols := [9]string{
+	cols := [10]string{
+		"calc",
 		"series",
 		"servers",
 		"groups",
@@ -116,7 +220,7 @@ func tryCount(lines *[]string, m map[string]interface{}) (bool, error) {
 		if count, ok := m[col]; ok {
 			i, ok := count.(int)
 			if ok {
-				*lines = append(*lines, fmt.Sprintf(`"%s",%d`, col, i))
+				*lines = append(*lines, fmt.Sprintf(`%s,%d`, col, i))
 				return true, nil
 			}
 		}
